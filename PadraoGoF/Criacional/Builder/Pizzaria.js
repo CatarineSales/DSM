a66e0b0b9430328a7b286babdeae893d68ef1158
CahSales <Aluno@fatecdiadema.com>
2024-03-08 10:49:25 -0300

    Add o primeiro commit

---
+++ PadraoGoF/Criacional/Builder/Pizzaria.js
@@ -0,0 +1,27 @@
+// ****************************************************************************
+// FATEC Diadema - Luigi Papaiz
+// Curso: Desenvolvimento de Sistema Multiplataformas - DSM
+// Disciplina: Tecnica de Programação II
+// Autor: Catarine Sales e Pedro Henrique        Data: 08/03/2024
+// Descrição: Aplicação do padrão GoF - Criacional - Builder
+// desenvolvido como exemplo na aula. 
+// ---------------------------------------------------------------------------
+// DESAFIO PROPOSTO: Desenvolver um programa com o uso do padrão de projeto GoF -
+// Criacional - Builder, em relação a uma pizzaria no qual deve conter:
+//       -Tamanho(Grande, Padrão e Pequeno);
+//       -Massa (Tradicional, Integral e Fitness);
+//       -Molho (Molho de Tomate, Molho Temperado e Molho Ervas);
+//       -Proteinas (Calabresa, Frango, Presunto, Atum);
+//       -Queijos (Mussarela, Catupiry, Cheddar);
+//       -Complementos (Azeitona, Oregano, Tomate, Cebola, Bacon);
+// 
+// -Montar o objeto da pizza com builder;
+// -Gerar um modelo default para "limpar" o pedido antes;
+// -O suário pode personalizar e no final deve mostrar como ficou
+// 
+// ****************************************************************************
+
+//DEFINIR PARTES DA PIZZA:
+
+
+      
